1cabf407812d96fc01acdb8f29c408bc5bde918f
RashaRahman <63566908+rasha-rahman123@users.noreply.github.com>
2020-08-17 23:09:21 -0700

    build

---
+++ precache-manifest.b2b219d96c51a598d1b5ea4f81efb78c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515a8a58ad51aa0c56c80d6eab7dde54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13018dc12fbc3dd55042",
+    "url": "/static/css/main.16e7baa5.chunk.css"
+  },
+  {
+    "revision": "02a2df13c6ce3d365712",
+    "url": "/static/js/2.2f834811.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2f834811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13018dc12fbc3dd55042",
+    "url": "/static/js/main.83d405e2.chunk.js"
+  },
+  {
+    "revision": "c757e7ec6e3c06b843a4",
+    "url": "/static/js/runtime-main.b7310f26.js"
+  }
+]);
